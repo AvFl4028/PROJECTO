cf4af1fd0ea8680de68ffd0bd0fbdd8c141d0622
AvielFl <aviel4028@gmail.com>
2021-05-12 23:06:46 -0500

    ahora si ya se arreglaron los bugs de vista de telefono y se agregaron algunas animaciones

--- funcion.js
+++ funcion.js
@@ -1,6 +1,28 @@
-var ventana;
+window.addEventListener('scroll', function(){
+    let animacion = this.document.getElementById('animar')
+    let posicionObj1 = animacion.getBoundingClientRect().top;
+    console.log(posicionObj1)
+    let tamañoDePantalla = window.innerHeight/3.5;
 
-var hola = "Hola!!!";
-ventana = prompt("Por favor pon tu nombre");
-
-alert(hola + " " + " " + ventana);
+    if(posicionObj1 < tamañoDePantalla){
+        animacion.style.animation = 'mover 1s ease-out'
+    }
+})
+ScrollReveal().reveal('.img1');
+ScrollReveal().reveal('.img2',{ delay: 500});
+ScrollReveal().reveal('.img3',{ delay: 500});
+ScrollReveal().reveal('.img4',{ delay: 500});
+ScrollReveal().reveal('.img5',{ delay: 500});
+ScrollReveal().reveal('.img6',{ delay: 500});
+ScrollReveal().reveal('.img7',{ delay: 500});
+ScrollReveal().reveal('.img8',{ delay: 500});
+ScrollReveal().reveal('.img9',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
+ScrollReveal().reveal('.',{ delay: 500});
